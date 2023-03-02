6e24f6d7ecddf3200d4fbab6a9d4e6fc24337a6f
sumitbera <sumit.bera@live.in>
2023-03-02 18:55:47 +1100

    Added public and private subnet range

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 #-------- root/main.tf -------------
 
 module "networking" {
-  source   = "./networking"
-  vpc_cidr = "10.16.0.0/16"
+  source        = "./networking"
+  vpc_cidr      = "10.16.0.0/16"
+  public_cidrs  = ["10.16.0.0/18", "10.16.64.0/18"]
+  private_cidrs = ["10.16.128.0/18", "10.16.192.0/18"]
 }

8bbe322cbc82e9ca5e36fc24772091b205e0053f
sumitbera <sumit.bera@live.in>
2023-03-03 17:12:11 +1100

    Moved local config vpc cidr to locals

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 #-------- root/main.tf -------------
 
-locals {
-  vpc_cidr = "10.16.0.0/16"
-}
-
-
 module "networking" {
   source           = "./networking"
   vpc_cidr         = local.vpc_cidr
   access_ip        = var.access_ip
+  security_groups  = local.security_groups
   private_sn_count = 2
   public_sn_count  = 2
   public_cidrs     = [for i in range(0, 2, 1) : cidrsubnet(local.vpc_cidr, 2, i)]

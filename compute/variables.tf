6ae6ff84d407f3c151b723c3d5984000e8b53f91
sumitbera <sumit.bera@live.in>
2023-03-03 21:04:22 +1100

    Added variables for ec2 instance

---
+++ compute/variables.tf
@@ -0,0 +1,8 @@
+# -------- compute/variables.tf ------------
+
+variable "instance_count" {}
+variable "instance_type" {}
+variable "volume_size" {}
+variable "public_sg" {}
+variable "public_subnets" {}
+variable "key_name" {}

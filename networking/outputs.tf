54d31aaaf96a11156d44d05dfd577050e5602752
sumitbera <sumit.bera@live.in>
2023-03-03 21:03:49 +1100

    Added output for subnet and sg

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -3,3 +3,11 @@
 output "vpc_id" {
     value = aws_vpc.dev_vpc.id
 }
+
+output "public_sg" {
+  value = aws_security_group.dev-sg["public"].id
+}
+
+output "public_subnets" {
+  value = aws_subnet.dev_public_subnet.*.id
+}

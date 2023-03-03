01fb5820a12d943c82c5dc3232e1e5f8086d0e90
sumitbera <sumit.bera@live.in>
2023-03-03 21:05:03 +1100

    Added compute for ec2 module

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ module "networking" {
   public_cidrs     = [for i in range(0, 2, 1) : cidrsubnet(local.vpc_cidr, 2, i)]
   private_cidrs    = [for i in range(2, 4, 1) : cidrsubnet(local.vpc_cidr, 2, i)]
 }
+
+module "compute" {
+  source         = "./compute"
+  public_sg      = module.networking.public_sg
+  public_subnets = module.networking.public_subnets
+  instance_count = 1
+  instance_type  = "t2.micro"
+  volume_size    = 10
+  key_name = "testaut"
+}

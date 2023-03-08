bd6f8ea137a719f695a1e4393685e8a12b1016c4
sumitbera <sumit.bera@live.in>
2023-03-08 22:48:13 +1100

    added ecs variables

---
+++ ecs/variables.tf
@@ -0,0 +1,12 @@
+variable "cluster_name" {
+  type = string
+}
+variable "service_name" {
+  type = string
+}
+variable "desired_count" {
+}
+variable "public_sg" {}
+variable "public_subnets" {}
+variable "launch_type" {}
+variable "platform_version" {}

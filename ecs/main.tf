0a4f20e03ec81334a20c6cd5518daeecf7c009ac
sumitbera <sumit.bera@live.in>
2023-03-09 21:35:23 +1100

    Added ecs capacity provider resource

--- ecs/main.tf
+++ ecs/main.tf
@@ -23,3 +23,14 @@ resource "aws_ecs_service" "load-test-service" {
     subnets = var.public_subnets
   }
 }
+
+resource "aws_ecs_cluster_capacity_providers" "ecs-provider" {
+  cluster_name = aws_ecs_cluster.load-test.id
+  capacity_providers = ["FARGATE_SPOT,FARGATE"]
+
+  default_capacity_provider_strategy {
+    base =1
+    weight = 100
+    capacity_provider = "FARGATE"
+  }
+}
